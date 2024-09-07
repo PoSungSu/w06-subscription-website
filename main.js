6655e4f3166ff2e1b54f2caf2c81b513dfb00072
Po Sung Su <r9268373@gmail.com>
2024-09-08 03:50:51 +0800

    swiper link 2nd commit

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
+{/* <script src="/node_modules/swiper/swiper-bundle.js"></script> */}
+// import 'swiper/swiper-bundle.js';
+// import * as myModule from "./node_modules/swiper/swiper-bundle";
 
 console.log("Hello world!");
