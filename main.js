b07d5daa900f356c9993b2bc76e5450f00ce7496
Po Sung Su <r9268373@gmail.com>
2024-09-08 20:31:11 +0800

    增加 aos 動畫 只有index

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
-{/* <script src="/node_modules/swiper/swiper-bundle.js"></script> */}
-// import 'swiper/swiper-bundle.js';
-// import * as myModule from "./node_modules/swiper/swiper-bundle";
+
+AOS.init();
 
 console.log("Hello world!");
